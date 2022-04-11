de413eb704ae4ab9ac482160d19f586776942083
Chandrakanth9394 <95459551+Chandrakanth9394@users.noreply.github.com>
2022-04-11 16:08:38 +0530

    Create tcl.sql

---
+++ course_outcome_1/TCL/tcl.sql
@@ -0,0 +1,17 @@
+create database tcl;
+use tcl;
+create table dept(deptno varchar(20) not null,dname varchar(20) not null,loc varchar(20) not null,primary key(deptno));
+insert into dept values("d001","finance","kollam");
+insert into dept values("d002","it","ernakulam");
+insert into dept values("d003","management","thrissur");
+set autocommit=0;
+insert into dept values("d004","it","kozhikode");
+savepoint b; 
+rollback;
+select *from dept;
+insert into dept values("d005","finance","kozhikode");
+savepoint c;
+insert into dept values("d006","finance","malappuram");
+savepoint d;
+rollback to c;
+commit;
